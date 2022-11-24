d33bb88da91263ec66f1fc0987eb034015e2931d
Dev <amine@sherpas.com>
2022-11-24 12:19:16 +0100

    add node server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Jenkins test code")
+const express = require('express')
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Jenkins TP7 EFREI!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
